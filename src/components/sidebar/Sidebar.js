bf898cde730a6be05317164fd743b6b1eaf8f970
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2024-07-26 17:41:27 +0900

    1:24:16

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,3 +1,17 @@
+import {
+  AccountCircleOutlined,
+  CreditCard,
+  Dashboard,
+  ExitToApp,
+  InsertChart,
+  LocalShipping,
+  NotificationsNone,
+  PersonOutline,
+  PsychologyOutlined,
+  SettingsApplications,
+  SettingsSystemDaydreamOutlined,
+  Store,
+} from "@mui/icons-material";
 import "./sidebar.scss";
 
 const Sidebar = () => {
@@ -6,23 +20,68 @@ const Sidebar = () => {
       <div className="top">
         <span className="logo">lamadmin</span>
       </div>
+      <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <Dashboard className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PersonOutline className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <Store className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCard className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShipping className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChart className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNone className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlined className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlined className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplications className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlined className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToApp className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
